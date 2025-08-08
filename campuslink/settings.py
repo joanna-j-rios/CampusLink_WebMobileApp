3ff2b91c753f1a5595f43abae93ce53c346d85b9
strwbtart <emmapach05@gmail.com>
2025-08-08 11:08:39 -0500

    now in installed apps

--- campuslink/settings.py
+++ campuslink/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bulletin',  # include the bulletin app
 ]
 
 MIDDLEWARE = [
